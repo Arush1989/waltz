2cbd523bdb7932b955996a9f2d20230acefe4fd0
Arush1989 <arush1989.srm@gmail.com>
2022-08-22 14:43:37 +1000

    brain fog i guess

--- waltz-web/src/main/java/com/khartec/waltz/web/WebUtilities.java
+++ waltz-web/src/main/java/com/khartec/waltz/web/WebUtilities.java
@@ -82,7 +82,7 @@ public class WebUtilities {
     }
     
     public static String testMethod(String... segs) {
-        return StringUtilities.testMethod(segs);
+        return StringUtilities.mkPath(segs);
     }
 
 
